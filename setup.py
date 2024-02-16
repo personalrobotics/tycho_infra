0040d5ef7f99f75c805762239bae8f6881b8eee5
PRL Desktop <prl@prl-desktop.com>
2024-02-15 19:43:28 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='tycho_infra',
+    version='1.0.0',
+    packages=['tycho_infra.mujoco_assets'],
+    license='MIT',
+)
---
+++ tycho_infra/mujoco_assets/TychoPhysics.py
@@ -0,0 +1,46 @@
+from dm_control import mujoco
+from .InverseKinematics import qpos_from_site_pose
+import numpy as np
+import os
+from scipy.spatial.transform import Rotation as R
+
+class TychoPhysics:
+    def __init__(self, joint_positions):
+        xml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets/hebi.xml')
+        self.physics = mujoco.Physics.from_xml_path(xml_path)
+        self.set_joint_positions(joint_positions)
+
+    def set_joint_positions(self, joint_positions):
+        with self.physics.reset_context():
+            self.physics.named.data.qpos["HEBI/base/X8_9"] = joint_positions[0]
+            self.physics.named.data.qpos["HEBI/shoulder/X8_16"] = joint_positions[1]
+            self.physics.named.data.qpos["HEBI/elbow/X8_9"] = joint_positions[2]
+            self.physics.named.data.qpos["HEBI/wrist1/X5_1"] = joint_positions[3]
+            self.physics.named.data.qpos["HEBI/wrist2/X5_1"] = joint_positions[4]
+            self.physics.named.data.qpos["HEBI/wrist3/X5_1"] = joint_positions[5]
+            self.physics.named.data.qpos["HEBI/chopstick/X5_1"] = joint_positions[6]
+
+    def get_IK(self, target_pos, target_quat, max_steps):
+        IK_all_joints = qpos_from_site_pose(
+            self.physics,
+            site_name="fixed_chop_tip_no_rot",
+            target_pos=target_pos,
+            target_quat=target_quat,
+            joint_names=np.array(["HEBI/base/X8_9", "HEBI/shoulder/X8_16", "HEBI/elbow/X8_9", "HEBI/wrist1/X5_1", "HEBI/wrist2/X5_1", "HEBI/wrist3/X5_1", "HEBI/chopstick/X5_1"]),
+            tol=0.0,
+            regularization_threshold=0.0,
+            regularization_strength=3e-2,
+            max_update_norm=0.004,
+            max_steps=max_steps,
+            inplace=True)
+
+        return IK_all_joints.qpos[0:7]
+    
+    def get_FK(self):
+        translation = self.physics.named.data.site_xpos["fixed_chop_tip_no_rot"]
+        euler = R.from_matrix(self.physics.named.data.site_xmat["fixed_chop_tip_no_rot"].reshape(3, -1)).as_euler("xyz")
+        actual_euler = [euler[0] - np.pi / 2, euler[1], euler[2]]
+        return np.hstack((translation, actual_euler))
+
+if __name__ == '__main__':
+    simulation_instance = TychoPhysics([-2.05810131, 1.41744053, 2.06072881, -0.9061228, 1.57976982, -2.04056325, -0.37010479])
